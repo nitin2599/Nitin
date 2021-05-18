5b60cc1371dfa712afba4c01c7da8b611a325b13
krishna-singh2 <krishna.singh2@delhivery.com>
2021-05-18 10:54:08 +0530

    added initial setup

---
+++ build/generated/openapi/src/main/java/org/openapitools/api/UserApiController.java
@@ -0,0 +1,24 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-05-18T10:53:20.574+05:30[Asia/Kolkata]")
+@Controller
+@RequestMapping("${openapi.userManagement.base-path:/v2}")
+public class UserApiController implements UserApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public UserApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
